83241346f25eac14bcb27acb3e7a73c261f67dc9
Gonzie <gonc.esteves@gmail.com>
2018-02-04 13:54:16 +0000

    add load balancer. Recode certain parts to increase code reusability

--- src/PDOLoadException.php
+++ src/PDOLoadException.php
@@ -1,8 +1,20 @@
 <?php
 
+/**
+ * This file is part of the Symfony package.
+ *
+ * (c) Gonzie <hello@gonz.ie>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace gonzie\PDOLoad;
 
-class PDOLoadException extends Exception
+/**
+ * PDOLoad
+ */
+class PDOLoadException extends \Exception
 {
-    
+
 }
